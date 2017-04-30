dad9d1055aba39ad3bb0f39c7fb03238d5bedfd2
Changfu Chen <cfchen1995@163.com>
2017-04-30 17:35:46 +0800

    Deleted the mineos_install.bash.

--- mineos_install.bash
+++
@@ -1,7 +0,0 @@
-git clone --recursive https://github.com/geodynamics/mineos.git
-git checkout --recursive https://github.com/geodynamics/mineos
-cd mineos
-autoreconf -i
-./configure --prefix=$HOME/cig
-make
-make install
