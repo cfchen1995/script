53aa47462356f5bc8eef2eef24a663e4cb8103d0
Changfu Chen <cfchen1995@163.com>
2017-04-30 17:40:13 +0800

    Add some descriptions.

--- mineos_install.sh
+++ mineos_install.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+# 源码安装mineos软件，进行软件的编译安装
+#
+# 04/30/2017 西南交通大学
+#
+
 git clone --recursive https://github.com/geodynamics/mineos.git
 git checkout --recursive https://github.com/geodynamics/mineos
 cd mineos
@@ -5,3 +11,4 @@ autoreconf -i
 ./configure --prefix=$HOME/cig
 make
 make install
+
